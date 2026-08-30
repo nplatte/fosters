0f22252a9810fce9a5818f4d34dbb9408278f6d0
Nathan Platte <nplatte0@gmail.com>
2026-08-30 17:39:14 -0400

    first test passes

--- litter/tests/test_views.py
+++ litter/tests/test_views.py
@@ -1,13 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class TestLitterGET(TestCase):
 
     def setUp(self):
+        self.url = reverse("landing")
         return super().setUp()
 
     def tearDown(self):
         return super().tearDown()
 
     def test_dashboard_returns_200(self):
-        pass
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
--- litter/urls.py
+++ litter/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from litter.views import *
 
 urlpatterns = [
+    path("", LandingPage.as_view(), name='landing')
 ]
