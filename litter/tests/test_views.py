ffdd1fb8f1ce0e6e271e0db915f2e6495421e524
Nathan Platte <nplatte0@gmail.com>
2026-08-30 17:34:55 -0400

    added first test

---
+++ litter/tests/test_views.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+
+class TestLitterGET(TestCase):
+
+    def setUp(self):
+        return super().setUp()
+
+    def tearDown(self):
+        return super().tearDown()
+
+    def test_dashboard_returns_200(self):
+        pass
