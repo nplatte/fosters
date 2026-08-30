4ed5103ae5b656c2bfee202ff8c63b5161846902
Nathan Platte <nplatte0@gmail.com>
2026-08-30 18:55:01 -0400

    added model fields

--- litter/models.py
+++ litter/models.py
@@ -1,41 +1,73 @@
 from django.db import models
-
+from datetime import date
 
 class Cat(models.Model):
 
-    pass
+    name = models.CharField(max_length=30)
+    estimated_date_of_birth = models.DateField(blank=True, null=True)
+    microchip = models.CharField(max_length=30, blank=True, null=True)
+    microchip_inserted_on = models.DateField(blank=True, null=True)
+    internal_id = models.CharField(max_length=30, blank=True, null=True)
+    gender = models.CharField(choices=[('male', 'male'), ('female', 'female')], max_length=30, blank=True, null=True)
+    color = models.CharField(max_length=30, blank=True, null=True)
+    litter = models.ForeignKey('Litter', on_delete=models.SET_NULL,blank=True, null=True)
 
 
 class Litter(models.Model):
 
-    pass
+    name = models.CharField(max_length=50, default="")
 
 
 class Vaccination(models.Model):
 
-    pass
+    name = models.CharField(max_length=50, default="")
+    brand = models.CharField(max_length=50, blank=True, null=True)
+    serial_number = models.CharField(max_length=50, blank=True, null=True)
+    date_given = models.DateField(default=date.today)
+    next_due = models.JSONField(blank=True, null=True)
 
 
 class Test(models.Model):
 
-    pass
+    name = models.CharField(max_length=50, default="")
+    date_administered = models.DateField(default=date.today)
+    result = models.CharField(max_length=100, blank=True, null=True)
 
 
 class Preventative(models.Model):
 
-    pass
+    type = models.CharField(max_length=30, default="")
+    brand = models.CharField(max_length=30, blank=True, null=True)
+    dosage = models.CharField(max_length=30, blank=True, null=True)
+    date_given = models.DateField(default=date.today)
+    next_due = models.DateField(blank=True, null=True)
+
 
 
 class Treatment(models.Model):
 
-    pass
+    type = models.CharField(max_length=30, default="")
+    issue = models.CharField(max_length=30, default="")
+    dosage = models.CharField(max_length=30, blank=True, null=True)
+    frequency = models.CharField(max_length=50, blank=True, null=True)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
 
 
 class Exam(models.Model):
 
-    pass
+    type = models.CharField(max_length=100, default="")
+    date_admitted = models.DateField(default=date.today)
+    date_picked_up = models.DateField(blank=True, null=True)
+    vet = models.CharField(max_length=50, blank=True, null=True)
+    taken_by = models.CharField(max_length=30, default="Kate")
+    notes = models.TextField(blank=True, null=True)
+
 
 
 class Weight(models.Model):
 
-    pass
+    weight = models.IntegerField(default=0)
+    unit = models.CharField(max_length=30, choices=[('g', 'g'), ('lb', 'lb')], default="lb")
+    date = models.DateField(default=date.today)
+    time = models.CharField(max_length=30, choices=[('am', 'am'), ('pm', 'pm')], blank=True, null=True)
--- litter/urls.py
+++ litter/urls.py
@@ -4,5 +4,5 @@ from litter.views import *
 
 urlpatterns = [
     path("", LandingView.as_view(), name='landing'),
-    path("/cat/add", AddCatView.as_view(), name='add_cat')
+    path("cat/add", AddCatView.as_view(), name='add_cat')
 ]
