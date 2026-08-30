c4f34244d3ae57d2f78ab26bdf2bdf48f8c186ca
Nathan Platte <nplatte0@gmail.com>
2026-08-30 18:24:18 -0400

    added model structure

--- litter/models.py
+++ litter/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Cat(models.Model):
+
+    pass
+
+
+class Litter(models.Model):
+
+    pass
+
+
+class Vaccination(models.Model):
+
+    pass
+
+
+class Test(models.Model):
+
+    pass
+
+
+class Preventative(models.Model):
+
+    pass
+
+
+class Treatment(models.Model):
+
+    pass
+
+
+class Exam(models.Model):
+
+    pass
+
+
+class Weight(models.Model):
+
+    pass
