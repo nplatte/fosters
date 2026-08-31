2bc33f262a5b84586253f075c1dc0096dd19fb61
Nathan Platte <nplatte0@gmail.com>
2026-08-31 13:45:11 -0400

    added test for add cat

--- fosters/settings.py
+++ fosters/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'litter',
+    'functional_tests',
 ]
 
 MIDDLEWARE = [
--- functional_tests/test_basics.py
+++ functional_tests/test_basics.py
@@ -1,15 +1,52 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.urls import reverse
 
 
 class TestCats(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.valid_data = {
+            "name": "Wiggles"
+        }
         return super().setUp()
 
+    def tearDown(self):
+        self.browser.close()
+        return super().tearDown()
+
     def test_can_add_cat(self):
-        pass
+        # the user logs into the website
+        self.browser.get(f'{self.live_server_url}{reverse('landing')}')
+        # the user is on the home page
+        self.assertEqual(self.browser.title, "Home Page")
+        # they click the view cats page
+        cats_link = self.browser.find_element(By.ID, "cats_link")
+        cats_link.click()
+        # they see some cats
+        self.assertEqual(self.browser.title, "Cats")
+        old_count = len(self.browser.find_elements(By.CLASS_NAME, "cat"))
+        # they click the add cat button
+        create_cat_link = self.browser.find_element(By.ID, "create_cat_link")
+        create_cat_link.click()
+        # they are taken to the add cat page
+        self.assertEqual(self.browser.title, "Add Cat")
+        # they enter in the cat inforamtion
+        for field_name, data in self.valid_data.items():
+            input = self.browser.find_element(By.ID, f'{field_name}_id')
+            input.send_keys(data)
+        # they click enter and are taken to the cat's page
+        submit_btn = self.browser.find_element(By.ID, "submit_btn")
+        submit_btn.click()
+        self.assertEqual(self.browser.title, f"{self.valid_data['name']}")
+        # they click cats and see the new cat added
+        cats_link = self.browser.find_element(By.ID, "cats_link")
+        cats_link.click()
+        new_count = len(self.browser.find_elements(By.CLASS_NAME, "cat"))
+        self.assertGreater(new_count, old_count)
 
     def test_can_view_cat(self):
         pass
