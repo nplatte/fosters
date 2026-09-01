7edc8f39e478efaacbc81968cac9c4c9a43273bd
Nathan Platte <nplatte0@gmail.com>
2026-09-01 06:26:13 -0400

    second test passes

--- functional_tests/test_basics.py
+++ functional_tests/test_basics.py
@@ -4,10 +4,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse
-from time import sleep
+from litter.models import Cat
 
 
 class TestCats(StaticLiveServerTestCase):
+
+    fixtures = [ "cats" ]
 
     @classmethod
     def setUpClass(cls):
@@ -63,7 +65,28 @@ class TestCats(StaticLiveServerTestCase):
         self.assertGreater(new_count, old_count)
 
     def test_can_view_cat(self):
-        self.fail("finish the test")
+        # the user logs in
+        self.browser.get(f'{self.live_server_url}{reverse('landing')}')
+        self.assertEqual(self.browser.title, "Home Page")
+        # they click the cats button
+        cats_link = self.browser.find_element(By.ID, "cats_link")
+        cats_link.click()
+        # they click the cat they want to see
+        cat_link = self.browser.find_element(By.ID, "cat_1_link")
+        cat_link.click()
+        test_cat = Cat.objects.get(pk=1)
+        self.assertEqual(self.browser.title, test_cat.name)
+        # they see the cats information
+        cat_ids = [
+            ("cat_name", test_cat.name), ("cat_dob", test_cat.estimated_date_of_birth.strftime("%Y-%m-%d")), 
+            ("cat_microchip", f"{test_cat.microchip} inserted on {test_cat.microchip_inserted_on.strftime("%Y-%m-%d")}"),
+            ("cat_internal", test_cat.internal_id), ("cat_gender", test_cat.gender), ("cat_color", test_cat.color),
+            ("cat_litter", str(test_cat.litter))
+        ]
+        for id in cat_ids:
+            element = self.browser.find_element(By.ID, id[0])
+            self.assertEqual(element.text, id[1], msg=f"{id[0]}")
+        # they log out
 
     def test_can_delete_cat(self):
         self.fail("finish the test")
