c8048356692fa09669ee72b65b6c2d126706601d
Nathan Platte <nplatte0@gmail.com>
2026-09-04 05:49:27 -0400

    moved event tests to new file

--- functional_tests/test_basics.py
+++ functional_tests/test_basics.py
@@ -1,10 +1,6 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse
-from litter.models import Cat, Event
+from litter.models import Cat
 from functional_tests.foster_helper import TestHelper
 
 
@@ -123,88 +119,5 @@ class TestCats(TestHelper):
         # they see the same number of cats
         new_count = self.count_elements_by_class("cat")
         self.assertEqual(new_count, old_count)
-
-
-class TestEvent(TestHelper):
-
-    fixtures = [ "cats", "events" ]
-
-    def setUp(self):
-        self.browser.delete_all_cookies()
-        self.valid_data = {
-            "weight": "123", "medications": "Clav, Vycodin",
-            "condition": "is super cute", "cat": f"{Cat.objects.get(pk=1).pk}",
-        }
-        return super().setUp()
-
-    def tearDown(self):
-        return super().tearDown()
-
-    def test_add_event(self):
-        # the user logs into the site
-        self.log_in_to_site()
-        # they want to add an event to a cat
-        # they go to the cat page
-        self.navigate_to_cats_page()
-        test_cat = Cat.objects.get(pk=1)
-        self.find_and_click(f"view_{test_cat.pk}_link")
-        self.assertEqual(self.browser.title, test_cat.name)
-        old_count = self.count_elements_by_class("event")
-        # they click add event
-        self.find_and_click("add_event")
-        self.assertEqual(self.browser.title, "Add Event")
-        # they enter the event info and hit submit
-        self.fill_in_form_by_ids(self.valid_data)
-        # they are taken to the cat page
-        self.assertEqual(self.browser.title, test_cat.name)
-        # they see the new event
-        new_count = self.count_elements_by_class("event")
-        self.assertGreater(new_count, old_count)
-        # they log off
-
-    def test_view_event(self):
-        cat = Cat.objects.get(pk=1)
-        # the user logs into the website
-        self.log_in_to_site()
-        # the user goes to the all cats page
-        self.navigate_to_cats_page()
-        # the user clicks on the cat with an event
-        self.find_and_click(f"view_{cat.pk}_link")
-        # they see events
-        got = self.count_elements_by_class("event")
-        cat_events = Event.objects.filter(cat=cat)
-        self.assertEqual(len(cat_events), got)
-        # they see the events info
-        event = cat_events.first()
-        for field, data in {"meds":event.medications, "weight":f"{event.weight} grams", "condition":event.condition}.items():
-            element = self.findElementByID(f"event_{event.pk}_{field}")
-            self.assertEqual(element.text, data)
-
-    def test_edit_event(self):
-        cat = Cat.objects.get(pk=1)
-        # the user logs into the website
-        self.log_in_to_site()
-        # the user goes to the all cats page
-        self.navigate_to_cats_page()
-        # the user clicks on the cat with an event
-        self.find_and_click(f"view_{cat.pk}_link")
-        self.assertTitleEquals(cat.name)
-        # they see events
-        count = self.count_elements_by_class("event")
-        self.assertGreater(count, 0)
-        event_weight = self.findElementByID("event_1_weight")
-        self.assertNotEqual(event_weight.text, "200 grams")
-        # they click the event edit button
-        self.find_and_click("event_1_edit")
-        self.assertTitleEquals("Edit Event")
-        # they update the weight
-        data = {"weight": "200"}
-        self.fill_in_form_by_ids(data)
-        # they are taken to the cat page again
-        self.assertTitleEquals(cat.name)
-        # they see the event weight is updated
-        event_weight = self.findElementByID("event_1_weight")
-        self.assertEqual(event_weight.text, "200 grams")
-
 
 
