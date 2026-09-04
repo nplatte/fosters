4ba2833794afb451f8c700757536f475d05363de
Nathan Platte <nplatte0@gmail.com>
2026-09-04 07:47:10 -0400

    did some refactoring, added EditView Test case

--- litter/tests/test_views/mixins.py
+++ litter/tests/test_views/mixins.py
@@ -1,4 +1,6 @@
-class DeleteViewTestCaseMixin:
+
+
+class BaseTestCaseMixin:
 
     fixtures = ['cats', 'events']
     tc_name = None
@@ -10,12 +12,27 @@ class DeleteViewTestCaseMixin:
         response = self.client.get(self.tc.url)
         self.assertEqual(response.status_code, 200)
 
+class RedirectTestCaseMixin(BaseTestCaseMixin):
+
+    def test_POST_success_rediret_to_page(self):
+        response = self.client.post(self.tc.url, self.tc.valid_data)
+        self.assertRedirects(response, self.tc.redirect_url)
+
+
+class EditViewTestCaseMixin(RedirectTestCaseMixin):
+
+    def test_POST_edits_model(self):
+        old_count = self.tc.model.objects.count()
+        self.client.post(self.tc.url, self.tc.valid_data)
+        new_count = self.tc.model.objects.count()
+        self.assertEqual(new_count, old_count)
+
+
+class DeleteViewTestCaseMixin(RedirectTestCaseMixin):
+
     def test_POST_deletes_model(self):
         old_count = self.tc.model.objects.count()
         self.client.post(self.tc.url, data=self.tc.valid_data)
         new_count = self.tc.model.objects.count()
         self.assertLess(new_count, old_count)
-
-    def test_POST_redirect_to_url(self):
-        response = self.client.post(self.tc.url, self.tc.valid_data)
-        self.assertRedirects(response, self.tc.redirect_url)
+        
--- litter/tests/test_views/test_generic_views.py
+++ litter/tests/test_views/test_generic_views.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.urls import reverse
 from litter.models import Cat, Event
 from django.test import TestCase
-from litter.tests.test_views.mixins import DeleteViewTestCaseMixin
+from litter.tests.test_views.mixins import DeleteViewTestCaseMixin, EditViewTestCaseMixin
 
 
 @dataclass
@@ -13,6 +13,27 @@ class AddViewTestCase:
     model: type[models.Model]
     valid_data: dict
     redirect_url: str
+
+class TestEditCatView(EditViewTestCaseMixin, TestCase):
+
+    def get_test_case(self):
+        return AddViewTestCase(
+        name="Edit Cat Test",
+        url = reverse('edit_cat', kwargs={"pk": 1}),
+        model=Cat,
+        valid_data={"name": "CAT"},
+        redirect_url=reverse("cat", kwargs={"pk": Cat.objects.count()}))
+
+
+class TestEditEventView(EditViewTestCaseMixin, TestCase):
+
+    def get_test_case(self):
+        return AddViewTestCase(
+        name="Add Event Test",
+        url=reverse('edit_event', kwargs={"pk": 1}),
+        model=Event,
+        valid_data={"weight": "312", "condition": "super cute", "cat": "1"},
+        redirect_url=reverse("cat", kwargs={"pk": Event.objects.count()}))
 
 
 class TestDeleteCatView(DeleteViewTestCaseMixin, TestCase):
